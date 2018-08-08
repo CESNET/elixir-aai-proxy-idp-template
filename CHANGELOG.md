77de43e2be5b7a7fdae1a60856221621aba571fb
Pavel Vyskočil <vyskocilpavel@muni.cz>
2018-08-08 14:45:06 +0200

    Changed the URL to AddInstitution App

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
  All notable changes to this project will be documented in this file.
  
  ## [Unreleased]
+ [Changed]
+ - Changed the URL to AddInstitution App
  
  ## [v1.0.2]
  [Changed]
--- themes/elixir/perun/disco-tpl.php
+++ themes/elixir/perun/disco-tpl.php
@@ -82,7 +82,7 @@ echo '<div class="no-idp-found alert alert-info">';
 if ($this->isOriginalSpNonFilteringIdPs()) {
 	echo $this->t('{elixir:elixir:cannot_find_institution}') . '<a href="mailto:aai-contact@elixir-europe.org?subject=Request%20for%20adding%20new%20IdP">aai-contact@elixir-europe.org</a>';
 } else {
-	echo $this->t('{elixir:elixir:cannot_find_institution_extended}') . '<a class="btn btn-primary" href="https://perun.elixir-czech.cz/add-institution/">' . $this->t('{elixir:elixir:add_institution}') . '</a>';
+	echo $this->t('{elixir:elixir:cannot_find_institution_extended}') . '<a class="btn btn-primary" href="https://login.elixir-czech.org/add-institution/">' . $this->t('{elixir:elixir:add_institution}') . '</a>';
 }
 echo '</div>';
 
