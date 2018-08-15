6a98a6bab7651fcd0bf70467b18a207988e89e7d
Dominik Baránek <0Baranek.dominik0@gmail.com>
2018-08-15 10:30:03 +0200

    Added warning in disco-tpl from a config

--- themes/elixir/perun/disco-tpl.php
+++ themes/elixir/perun/disco-tpl.php
@@ -17,74 +17,134 @@ $this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML\Modul
 
 $this->data['head'] .= searchScript();
 
+const WARNING_CONFIG_FILE_NAME = 'config-warning.php';
+const WARNING_IS_ON = 'isOn';
+const WARNING_USER_CAN_CONTINUE = 'userCanContinue';
+const WARNING_TITLE = 'title';
+const WARNING_TEXT = 'text';
 
+$warningIsOn = false;
+$warningUserCanContinue = null;
+$warningTitle = null;
+$warningText = null;
+$config = null;
+
+try {
+	$config = SimpleSAML_Configuration::getConfig(WARNING_CONFIG_FILE_NAME);
+} catch (Exception $ex) {
+	SimpleSAML\Logger::warning("elixir:disco-tpl: missing or invalid config-warning file");
+}
+
+if ($config != null) {
+	try {
+		$warningIsOn = $config->getBoolean(WARNING_IS_ON);
+	} catch (Exception $ex) {
+		SimpleSAML\Logger::warning("elixir:disco-tpl: missing or invalid isOn parameter in config-warning file");
+		$warningIsOn = false;
+	}
+}
+
+if ($warningIsOn) {
+	try {
+		$warningUserCanContinue = $config->getBoolean(WARNING_USER_CAN_CONTINUE);
+	} catch (Exception $ex) {
+		SimpleSAML\Logger::warning("elixir:disco-tpl: missing or invalid userCanContinue parameter in config-warning file");
+		$warningUserCanContinue = true;
+	}
+	try {
+		$warningTitle = $config->getString(WARNING_TITLE);
+		$warningText = $config->getString(WARNING_TEXT);
+		if (empty($warningTitle) || empty($warningText)) {
+			throw new Exception();
+		}
+	} catch (Exception $ex) {
+		SimpleSAML\Logger::warning("elixir:disco-tpl: missing or invalid title or text in config-warning file");
+		$warningIsOn = false;
+	}
+}
+
+
+if ($warningIsOn && !$warningUserCanContinue) {
+	$this->data['header'] = $this->t('{elixir:elixir:warning}');
+}
 
 $this->includeAtTemplateBase('includes/header.php');
 
 
-
-if (!empty($this->getPreferredIdp())) {
-
-	echo '<p class="descriptionp">' . $this->t('{elixir:elixir:previous_selection}') . '?></p>';
-	echo '<div class="metalist list-group">';
-	echo showEntry($this, $this->getPreferredIdp(), true);
+if ($warningIsOn) {
+	if($warningUserCanContinue) {
+		echo '<div class="alert alert-warning">';
+	} else {
+		echo '<div class="alert alert-danger">';
+	}
+	echo '<h4> <strong>' . $warningTitle . '</strong> </h4>';
+	echo $warningText;
 	echo '</div>';
-
-
-	echo getOr($this);
 }
 
+if (!$warningIsOn || $warningUserCanContinue) {
+	if (!empty($this->getPreferredIdp())) {
+
+		echo '<p class="descriptionp">' . $this->t('{elixir:elixir:previous_selection}') . '</p>';
+		echo '<div class="metalist list-group">';
+		echo showEntry($this, $this->getPreferredIdp(), true);
+		echo '</div>';
+
+
+		echo getOr($this);
+	}
 
 
 
-echo '<div class="row">';
-foreach ($this->getIdps('social') AS $idpentry) {
+    echo '<div class="row">';
+    foreach ($this->getIdps('social') AS $idpentry) {
 
-	echo '<div class="col-md-4">';
-	echo '<div class="metalist list-group">';
-	echo showEntry($this, $idpentry, false);
-	echo '</div>';
-	echo '</div>';
+        echo '<div class="col-md-4">';
+        echo '<div class="metalist list-group">';
+        echo showEntry($this, $idpentry, false);
+        echo '</div>';
+        echo '</div>';
 
-}
-echo '</div>';
-
-
-
-echo getOr($this);
+    }
+    echo '</div>';
 
 
 
-echo '<p class="descriptionp">';
-echo 'your institutional account';
-echo '</p>';
+    echo getOr($this);
 
-echo '<div class="inlinesearch">';
-echo '	<form id="idpselectform" action="?" method="get">
+
+
+    echo '<p class="descriptionp">';
+    echo 'your institutional account';
+    echo '</p>';
+
+    echo '<div class="inlinesearch">';
+    echo '	<form id="idpselectform" action="?" method="get">
 			<input class="inlinesearchf form-control input-lg" placeholder="Type the name of your institution" 
 			type="text" value="" name="query" id="query" autofocus oninput="document.getElementById(\'list\').style.display=\'block\';"/>
 		</form>';
-echo '</div>';
+    echo '</div>';
 
 
-echo '<div class="metalist list-group" id="list">';
-foreach ($this->getIdps() AS $idpentry) {
-	echo showEntry($this, $idpentry, false);
+    echo '<div class="metalist list-group" id="list">';
+    foreach ($this->getIdps() AS $idpentry) {
+        echo showEntry($this, $idpentry, false);
+    }
+    echo '</div>';
+
+
+    echo '<br>';
+    echo '<br>';
+
+
+    echo '<div class="no-idp-found alert alert-info">';
+    if ($this->isOriginalSpNonFilteringIdPs()) {
+        echo $this->t('{elixir:elixir:cannot_find_institution}') . '<a href="mailto:aai-contact@elixir-europe.org?subject=Request%20for%20adding%20new%20IdP">aai-contact@elixir-europe.org</a>';
+    } else {
+        echo $this->t('{elixir:elixir:cannot_find_institution_extended}') . '<a class="btn btn-primary" href="https://login.elixir-czech.org/add-institution/">' . $this->t('{elixir:elixir:add_institution}') . '</a>';
+    }
+    echo '</div>';
 }
-echo '</div>';
-
-
-echo '<br>';
-echo '<br>';
-
-
-echo '<div class="no-idp-found alert alert-info">';
-if ($this->isOriginalSpNonFilteringIdPs()) {
-	echo $this->t('{elixir:elixir:cannot_find_institution}') . '<a href="mailto:aai-contact@elixir-europe.org?subject=Request%20for%20adding%20new%20IdP">aai-contact@elixir-europe.org</a>';
-} else {
-	echo $this->t('{elixir:elixir:cannot_find_institution_extended}') . '<a class="btn btn-primary" href="https://login.elixir-czech.org/add-institution/">' . $this->t('{elixir:elixir:add_institution}') . '</a>';
-}
-echo '</div>';
 
 
 
